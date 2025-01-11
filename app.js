961b166b0dd8e7ac440c56118b341201f483b3e8
IgniteConvention <ignitestudentconvention@gmail.com>
2025-01-11 16:28:50 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Ignite Student Convention</h1>
+      <p>Start building your app here!</p>
+    </div>
+  );
+}
+
+export default App;
