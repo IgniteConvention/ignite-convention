ab9d631f3090f7cdbc5e38c6b7e6a9a423b89ca1
IgniteConvention <ignitestudentconvention@gmail.com>
2025-01-11 16:42:21 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import React from "react";
+import RegistrationForm from "./components/RegistrationForm";
+import EventList from "./components/EventList";
 
 function App() {
+  const events = ["Soccer Kick", "Track and Field", "Bible Bowl", "Choir"];
+
   return (
     <div>
-      <h1>Welcome to Ignite Student Convention</h1>
-      <p>Start building your app here!</p>
+      <h1>Ignite Student Convention</h1>
+      <RegistrationForm />
+      <EventList events={events} />
     </div>
   );
 }
